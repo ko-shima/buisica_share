2f06394b8ac6b60bd8eb074dab842a3c67927d48
ko-shima <xxghapxx.poomomo@gmail.com>
2024-01-27 14:52:14 +0900

    usersテーブルの作成

--- db/migrate/20240121090319_devise_create_users.rb
+++ db/migrate/20240121090319_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :last_name,          null: false
+      t.string  :first_name,         null: false
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :employee_number,    null: false
+      t.integer :department,         null: false
+      t.string  :phone_number,       null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
