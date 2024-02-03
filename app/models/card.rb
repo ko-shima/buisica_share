ad699e7f1d05e530364b2e6088e054162dd50589
ko-shima <xxghapxx.poomomo@gmail.com>
2024-02-03 22:22:24 +0900

    一覧表示画面の修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,13 +2,14 @@ class Card < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  #with_options presence: true do
-    #validates :client_last_name
-    #validates :client_first_name
-    #validates :company_name
-    #validates :client_email
-    #validates :client_department
-    #validates :client_phone_number
-    #validates :user_id
-  #end
+   with_options presence: true do
+     validates :client_last_name
+     validates :client_first_name
+     validates :company_name
+     validates :client_email
+     validates :client_department
+     validates :client_phone_number
+     validates :user_id
+     validates :image
+   end
 end
