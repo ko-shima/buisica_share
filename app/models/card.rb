657f942db132a422173db7452f142bc15859f033
ko-shima <xxghapxx.poomomo@gmail.com>
2024-02-11 15:55:06 +0900

    アソシエーションの設定
    コメントーユーザー
    コメントーカード

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,7 @@
 class Card < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments
 
   with_options presence: true do
     validates :client_last_name
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :department
   has_many :cards
+  has_many :comments
 
   # 空の投稿を保存できないようにする
   with_options presence: true do
