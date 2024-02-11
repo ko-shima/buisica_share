d7db03429391581adfa3bf760abf8a48e08f9587
ko-shima <xxghapxx.poomomo@gmail.com>
2024-02-11 18:05:44 +0900

    コメント投稿機能の実装完了

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/cards/#{comment.card.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, card_id: params[:card_id])
+  end
+end
